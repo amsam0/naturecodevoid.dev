152efab0eb3ebcee044f5d6f4fa670ec10bbd76d
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-09-21 11:09:42 -0700

    :zap: upgrade the scripts

--- scripts/copySite.bat
+++ scripts/copySite.bat
@@ -1,14 +1,12 @@
-call cd /D "%~dp0"
+cd /D "%~dp0"
 
-call cmd /c "buildLocal.bat"
-:: Copy to the site copy
-call robocopy ../_site ../../naturecodevoid.github.io_siteCopy /MIR
-:: Copy the gh-pages git folder
-call robocopy ../../naturecodevoid.github.io_gh-pages/.git ../../naturecodevoid.github.io_siteCopy/.git /MIR
-:: Copy to gh-pages
-call robocopy ../_site ../../naturecodevoid.github.io_gh-pages /MIR
-:: Copy .git folder to gh-pages
-call robocopy ../../naturecodevoid.github.io_siteCopy/.git ../../naturecodevoid.github.io_gh-pages/.git /MIR
-call cd ../
-:: call git checkout gh-pages
-call cd ../
+set SITE_NAME=naturecodevoid.github.io
+
+cmd /c "buildLocal.bat"
+
+cd /D "%~dp0"
+
+robocopy /S /V ../_site ../../%SITE_NAME%_siteCopy /MIR
+robocopy /S /V ../../%SITE_NAME%_gh-pages/.git ../../%SITE_NAME%_siteCopy/.git /MIR
+robocopy /S /V ../_site ../../%SITE_NAME%_gh-pages /MIR
+robocopy /S /V ../../%SITE_NAME%_siteCopy/.git ../../%SITE_NAME%_gh-pages/.git /MIR
--- scripts/copySite.sh
+++ scripts/copySite.sh
@@ -2,11 +2,13 @@
 
 cd "$PWD"
 
+SITE_NAME="naturecodevoid.github.io"
+
 sh ./buildLocal.sh
 
 cd "$PWD"
 
-cp -v -f -R "../_site/." "../../naturecodevoid.github.io_siteCopy"
-cp -v -f -R "../../naturecodevoid.github.io_gh-pages/.git/." "../../naturecodevoid.github.io_siteCopy/.git"
-cp -v -f -R "../_site/." "../../naturecodevoid.github.io_gh-pages"
-cp -v -f -R "../../naturecodevoid.github.io_siteCopy/.git/." "../../naturecodevoid.github.io_gh-pages/.git"
+cp -v -f -R "../_site/." "../../$(echo $SITE_NAME)_siteCopy"
+cp -v -f -R "../../$(echo $SITE_NAME)_gh-pages/.git/." "../../$(echo $SITE_NAME)_siteCopy/.git"
+cp -v -f -R "../_site/." "../../$(echo $SITE_NAME)_gh-pages"
+cp -v -f -R "../../$(echo $SITE_NAME)_siteCopy/.git/." "../../$(echo $SITE_NAME)_gh-pages/.git"
