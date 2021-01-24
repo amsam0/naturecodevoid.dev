cb6189565aae6e9f6e48cb19558a660db59779b2
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2021-01-24 09:12:16 -0800

    Update copySite.sh

--- scripts/copySite.sh
+++ scripts/copySite.sh
@@ -11,5 +11,6 @@ cd "$PWD"
 
 cp -v -f -R "../_site/." "../../$(echo $SITE_NAME)_siteCopy"
 cp -v -f -R "../../$(echo $SITE_NAME)_gh-pages/.git/." "../../$(echo $SITE_NAME)_siteCopy/.git"
+rm -v -f -R "../../$(echo $SITE_NAME)_gh-pages"
 cp -v -f -R "../_site/." "../../$(echo $SITE_NAME)_gh-pages"
 cp -v -f -R "../../$(echo $SITE_NAME)_siteCopy/.git/." "../../$(echo $SITE_NAME)_gh-pages/.git"
