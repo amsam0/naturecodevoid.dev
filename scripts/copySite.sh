f87460978ebfe60fdf908c04f6a050a383036124
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2021-03-19 09:28:43 -0700

    Update copySite.sh

--- scripts/copySite.sh
+++ scripts/copySite.sh
@@ -7,7 +7,8 @@ SITE_NAME="naturecodevoid.github.io"
 
 sh ./buildLocal.sh
 
-cd "$PWD"
+# https://stackoverflow.com/a/246128
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cp -v -f -R "../_site/." "../../$(echo $SITE_NAME)_siteCopy"
 cp -v -f -R "../../$(echo $SITE_NAME)_gh-pages/.git/." "../../$(echo $SITE_NAME)_siteCopy/.git"
